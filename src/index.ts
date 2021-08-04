354d5baa2479be82f717f450a32d2aa55e3b438f
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2021-08-05 00:07:32 +0900

    add: jwt auth

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { addResolversToSchema } from '@graphql-tools/schema';
 import { ApolloServer } from 'apollo-server';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
 import { join } from 'path';
 import resolvers from './resolvers';
 
@@ -12,16 +14,55 @@ const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
 const server = new ApolloServer({
   schema: schemaWithResolvers,
   context: async (ctx) => {
-    // TODO: tokenをdecodeしてsub取得
-    const token = ctx.req.headers.authorization;
-    console.log({ token });
-    return {
-      user: {
-        id: '1',
-        name: 'eringiv3',
-        email: '',
-      },
-    };
+    const token = ctx.req.headers.authorization?.replace('Bearer ', '');
+    if (token === undefined) {
+      return {
+        user: undefined,
+      };
+    }
+
+    try {
+      const user = await new Promise<JwtPayload>((resolve, reject) => {
+        const client = jwksClient({
+          jwksUri: `https://dev-f-4x-fhm.jp.auth0.com/.well-known/jwks.json`,
+        });
+        jwt.verify(
+          token,
+          (header, cb) => {
+            client.getSigningKey(header.kid, function (err, key) {
+              const signingKey = key.getPublicKey();
+              cb(null, signingKey);
+            });
+          },
+          {
+            audience: 'https://apollo-server-prisma-todo-app.eringiv3.com',
+            issuer: `https://dev-f-4x-fhm.jp.auth0.com/`,
+            algorithms: ['RS256'],
+          },
+          (err, decoded) => {
+            if (err) {
+              return reject(err);
+            }
+            if (decoded === undefined) {
+              return reject('decoded is invalid.');
+            }
+            resolve(decoded);
+          }
+        );
+      });
+      console.log({
+        user,
+      });
+      return {
+        user: {
+          id: user.sub,
+        },
+      };
+    } catch (error) {
+      return {
+        user: undefined,
+      };
+    }
   },
 });
 
