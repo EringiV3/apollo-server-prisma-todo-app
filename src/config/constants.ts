5d9698abe62285bd237f1eef1c0dcfe2a748c8a0
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2021-09-05 23:32:37 +0900

    fix

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,7 @@
-export const APOLLO_SERVER_PORT = process.env.PORT ?? '8080';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const APOLLO_SERVER_PORT = process.env.PORT ?? '4000';
 export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN ?? '';
 export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE ?? '';
